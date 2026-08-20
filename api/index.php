44c9c5e4795c96be43665692bfc112593c6e778c
saruban-01 <ssaruban75@gmail.com>
2026-08-20 21:30:57 +0530

    Remove debug exception logging

--- api/index.php
+++ api/index.php
@@ -4,9 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 ini_set('log_errors', '1');
 ini_set('error_log', 'php://stderr');
 
